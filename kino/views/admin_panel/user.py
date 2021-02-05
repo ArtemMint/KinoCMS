f2751d6b08f77e1f1d621f56c5b909a198e36534
ArtemMint <nemnovus@gmail.com>
2021-02-05 21:57:13 +0200

    KinoCMS:Added User Profile update

--- kino/views/admin_panel/user.py
+++ kino/views/admin_panel/user.py
@@ -11,7 +11,7 @@ from django.http import Http404
 
 def adminUserListView(request):
     contact_list = User.objects.all().order_by('-id')
-    paginator = Paginator(contact_list, 5)  # Show 25 contacts per page.
+    paginator = Paginator(contact_list, 5)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
