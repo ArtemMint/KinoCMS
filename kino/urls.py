e6327d51d345994dbe3f9b71d9f7717a54471203
ivanhumenyuk <vanya19960529@gmail.com>
2021-02-09 12:48:49 +0200

    admin banners adding

--- kino/urls.py
+++ kino/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views.admin_panel.banners import AdminBannersView
+from .views.admin_panel.banners import *
 from .views.admin_panel.cinema import *
 from .views.admin_panel.film import *
 from .views.admin_panel.home import admin_view
@@ -16,103 +16,102 @@ from .views.admin_panel.user import *
 from .views.site.cinema import *
 from .views.site.film import *
 from .views.site.home import HomeView
-from .views.site.news import *
+from .views.site.news import NewsView
 from .views.site.poster import PosterView
 from .views.site.shares import *
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('poster/', PosterView.as_view(), name='poster'),
-    path('premiere/', FilmPremiereView.as_view(), name='premiere'),
-    path('film/<int:pk>', FilmDetailView.as_view(), name='film_detail'),
-    path('cinemas/', CinemasView.as_view(), name='cinemas'),
-    path('cinema/<int:cinema_id>', cinemaDetailView, name='cinema_detail'),
-    path('cinema/<int:cinema_id>/hall/<int:cinemahall_id>', cinemahallDetailView, name='cinemahall_detail'),
-    path('shares/', sharesView, name='shares'),
-    path('shares/<int:shares_id>', sharesDetailView, name='shares_detail'),
-    path('news/', newsView, name='news'),
-    path('news/<int:news_id>', newsDetailView, name='news_detail'),
+                  path('', HomeView.as_view(), name='home'),
+                  path('poster/', PosterView.as_view(), name='poster'),
+                  path('premiere/', FilmPremiereView.as_view(), name='premiere'),
+                  path('film/<int:pk>', FilmDetailView.as_view(), name='film_detail'),
+                  path('cinemas/', CinemasView.as_view(), name='cinemas'),
+                  path('cinema/<int:pk>', CinemaDetailView.as_view(), name='cinema_detail'),
+                  path('shares/', SharesView.as_view(), name='shares'),
+                  path('news/', NewsView.as_view(), name='news'),
 
-    # Admin USERS
-    path('admin/users/', adminUserListView, name='admin_users'),
-    path('admin/users/create/',
-         adminUserCreateView, name='admin_create_users'),
-    path('admin/users/edit/<int:user_id>',
-         adminUserUpdateView, name='admin_update_users'),
-    path('admin/users/<int:user_id>/delete',
-         adminUserDeleteView, name='admin_delete_user'),
-    path('admin/', admin_view, name='admin_home'),
-    path('admin/users/', adminUserListView, name='admin_users'),
+                  # Admin USERS
+                  path('admin/users/', adminUserListView, name='admin_users'),
+                  path('admin/users/edit/<int:user_id>',
+                       adminUserUpdateView, name='admin_update_users'),
+                  path('admin/users/<int:pk>/delete',
+                       AdminUserDeleteView.as_view(), name='admin_delete_user'),
 
-    # Admin
-    path('admin/', admin_view, name='admin_home'),
-    path('admin/statistics/', adminStatisticsView,
-         name='admin_statistics'),
-    path('admin/banners/', AdminBannersView.as_view(),
-         name='admin_banners'),
-    path('admin/shares/', AdminSharesView.as_view(),
-         name='admin_shares'),
-    path('admin/pages/', AdminPagesView.as_view(),
-         name='admin_pages'),
-    path('admin/mailing/', AdminMailingView.as_view(),
-         name='admin_mailing'),
-    path('admin/mailing/', AdminMailingView.as_view(),
-         name='admin_mailing'),
+                  # Admin
+                  path('admin/', admin_view, name='admin_home'),
+                  path('admin/users/', adminUserListView, name='admin_users'),
 
-    # FILMS
-    path('admin/films/', AdminFilmsView.as_view(),
-         name='admin_films'),
-    path('admin/films/<int:pk>',
-         AdminFilmDetailView.as_view(), name='admin_film_detail'),
-    path('admin/films/edit/<int:pk>',
-         AdminFilmUpdateView.as_view(), name='admin_update_film'),
-    path('admin/films/<int:pk>/delete',
-         AdminFilmDeleteView.as_view(), name='admin_delete_film'),
-    path('admin/new_film/', AdminFilmAddView.as_view(),
-         name='admin_add_film'),
+                  # Admin
+                  path('admin/', admin_view, name='admin_home'),
+                  path('admin/statistics/', AdminStatisticsView.as_view(),
+                       name='admin_statistics'),
+                  path('admin/banners/sliders', slider_banners_update,
+                       name='admin_banners_sliders'),
+                  path('admin/banners/back', back_banners_update,
+                       name='admin_banners_back'),
+                  path('admin/banners/shares', shares_banners_update,
+                       name='admin_banners_shares'),
+                  path('admin/shares/', AdminSharesView.as_view(),
+                       name='admin_shares'),
+                  path('admin/pages/', AdminPagesView.as_view(),
+                       name='admin_pages'),
+                  path('admin/mailing/', AdminMailingView.as_view(),
+                       name='admin_mailing'),
+                  path('admin/mailing/', AdminMailingView.as_view(),
+                       name='admin_mailing'),
 
-    # CINEMA
-    path('admin/cinemas/', AdminCinemasView.as_view(),
-         name='admin_cinemas'),
-    path('admin/cinemas/<int:cinema_id>/',
-         adminCinemaDetailView, name='admin_cinema_detail'),
-    path('admin/cinemas/edit/<int:pk>/',
-         AdminCinemaUpdateView.as_view(), name='admin_update_cinema'),
-    path('admin/cinemas/<int:pk>/delete/',
-         AdminCinemaDeleteView.as_view(), name='admin_delete_cinema'),
-    path('admin/new_cinemas/', AdminCinemaAddView.as_view(),
-         name='admin_add_cinema'),
-    path('admin/cinemas/<int:cinema_id>/cinemahall/<int:cinemahall_id>/',
-         adminCinemahallDetailView, name='admin_cinemahall_detail'),
-    path('admin/cinemas/<int:cinema_id>/cinemahall/<int:cinemahall_id>/update/',
-         adminCinemahallUpdateView, name='admin_update_cinemahall'),
-    path('admin/cinemas/<int:cinema_id>/cinemahall/<int:cinemahall_id>/delete/',
-         adminCinemahallDeleteView, name='admin_delete_cinemahall'),
-    path('admin/cinemas/<int:cinema_id>/new_cinemahall/',
-         adminCinemahallCreateView, name='admin_add_cinemahall'),
+                  # FILMS
+                  path('admin/films/', AdminFilmsView.as_view(),
+                       name='admin_films'),
+                  path('admin/films/<int:pk>',
+                       AdminFilmDetailView.as_view(), name='admin_film_detail'),
+                  path('admin/films/edit/<int:pk>',
+                       AdminFilmUpdateView.as_view(), name='admin_update_film'),
+                  path('admin/films/<int:pk>/delete',
+                       AdminFilmDeleteView.as_view(), name='admin_delete_film'),
+                  path('admin/new_film/', AdminFilmAddView.as_view(),
+                       name='admin_add_film'),
 
-    # News
-    path('admin/news/', AdminNewsView.as_view(), name='admin_news'),
-    path('admin/news/<int:pk>', AdminNewsDetailView.as_view(),
-         name='admin_news_detail'),
-    path('admin/news/edit/<int:pk>',
-         AdminNewsUpdateView.as_view(), name='admin_update_news'),
-    path('admin/news/<int:pk>/delete',
-         AdminNewsDeleteView.as_view(), name='admin_delete_news'),
-    path('admin/new_news/', AdminNewsAddView.as_view(),
-         name='admin_add_news'),
+                  # CINEMA
+                  path('admin/cinemas/', AdminCinemasView.as_view(),
+                       name='admin_cinemas'),
+                  path('admin/cinemas/<int:cinema_id>/',
+                       adminCinemaDetailView, name='admin_cinema_detail'),
+                  path('admin/cinemas/edit/<int:pk>/',
+                       AdminCinemaUpdateView.as_view(), name='admin_update_cinema'),
+                  path('admin/cinemas/<int:pk>/delete/',
+                       AdminCinemaDeleteView.as_view(), name='admin_delete_cinema'),
+                  path('admin/new_cinemas/', AdminCinemaAddView.as_view(),
+                       name='admin_add_cinema'),
+                  path('admin/cinemas/<int:cinema_id>/cinemahall/<int:cinemahall_id>/',
+                       adminCinemahallDetailView, name='admin_cinemahall_detail'),
+                  path('admin/cinemas/<int:cinema_id>/cinemahall/<int:cinemahall_id>/update/',
+                       adminCinemahallUpdateView, name='admin_update_cinemahall'),
 
+                  path('admin/cinemas/<int:cinema_id>/new_cinemahall/', adminNewCinemahallView,
+                       name='admin_add_cinemahall'),
 
-    # Shares
-    path('admin/shares/', AdminSharesView.as_view(),
-         name='admin_shares'),
-    path('admin/shares/<int:pk>', AdminSharesDetailView.as_view(),
-         name='admin_shares_detail'),
-    path('admin/shares/edit/<int:pk>',
-         AdminSharesUpdateView.as_view(), name='admin_update_shares'),
-    path('admin/shares/<int:pk>/delete',
-         AdminSharesDeleteView.as_view(), name='admin_delete_shares'),
-    path('admin/new_shares/', AdminSharesAddView.as_view(),
-         name='admin_add_shares'),
+                  # News
+                  path('admin/news/', AdminNewsView.as_view(), name='admin_news'),
+                  path('admin/news/<int:pk>', AdminNewsDetailView.as_view(),
+                       name='admin_news_detail'),
+                  path('admin/news/edit/<int:pk>',
+                       AdminNewsUpdateView.as_view(), name='admin_update_news'),
+                  path('admin/news/<int:pk>/delete',
+                       AdminNewsDeleteView.as_view(), name='admin_delete_news'),
+                  path('admin/new_news/', AdminNewsAddView.as_view(),
+                       name='admin_add_news'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  # Shares
+                  path('admin/shares/', AdminSharesView.as_view(),
+                       name='admin_shares'),
+                  path('admin/shares/<int:pk>', AdminSharesDetailView.as_view(),
+                       name='admin_shares_detail'),
+                  path('admin/shares/edit/<int:pk>',
+                       AdminSharesUpdateView.as_view(), name='admin_update_shares'),
+                  path('admin/shares/<int:pk>/delete',
+                       AdminSharesDeleteView.as_view(), name='admin_delete_shares'),
+                  path('admin/new_shares/', AdminSharesAddView.as_view(),
+                       name='admin_add_shares'),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
