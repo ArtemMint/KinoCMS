53a0198dd65b5a0d19943f58cca67d7bf60e5b9c
ArtemMint <nemnovus@gmail.com>
2021-02-15 08:46:20 +0200

    fixed user create in Admin

--- register/signals.py
+++ register/signals.py
@@ -7,7 +7,8 @@ from register.models.client import Client
 @receiver(post_save, sender=User)
 def create_client(sender, instance, created, **kwargs):
     if created:
-        Client.objects.create(user=instance)
+        Client.objects.create(user=instance, address='', num_card='',
+                              birth_date=None, gender='', language='', city='')
 
 
 @receiver(post_save, sender=User)
