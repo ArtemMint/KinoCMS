ba3c01f8d63eca56e8209132751e7a8d615898b1
ArtemMint <nemnovus@gmail.com>
2021-02-16 17:09:45 +0200

    updated kino/views/site/home.py

--- kino/views/site/home.py
+++ kino/views/site/home.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import redirect, render, \
+    get_object_or_404
 
 from kino.models.film import Film
 from kino.models.image import FilmImage
@@ -6,11 +7,14 @@ from kino.models.pages import HomePage
 
 
 def home_page_view(request):
-    
-    films = Film.objects.all()
-    gallery = FilmImage.objects.filter(film=1) # should be slider banner on Home page
-    home_page = HomePage.objects.get(id=0)   
 
-    context = {"films": films, "home_page": home_page, 'gallery': gallery}
+    films = Film.objects.all()
+    # should be slider banner on Home page
+    gallery = FilmImage.objects.filter(film=1)
+    home_page = HomePage.objects.get(id=0)
+
+    context = {
+        "films": films, "home_page": home_page, 'gallery': gallery
+    }
     template_name = 'kino/home.html'
     return render(request, template_name, context)
