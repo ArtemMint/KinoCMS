563755e9307c384077a825e0f5a08dfc4480a3a2
ivanhumenyuk <vanya19960529@gmail.com>
2021-02-16 10:20:28 +0200

    admin pages changes

--- kino/forms/pages.py
+++ kino/forms/pages.py
@@ -9,7 +9,7 @@ class HomepageForm(forms.ModelForm):
         widgets = {
             'phone1': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '+XX XXX XXX XX XX'}),
             'phone2': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '+XX XXX XXX XX XX'}),
-            'seo_text': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Seo text..'}),
+            'seo_text': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Seo text...'}),
             'seo_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'SEO title of the cinema...'}),
             'seo_keywords': forms.TextInput(
                 attrs={'class': 'form-control', 'placeholder': 'SEO keywords of the cinema'}),
@@ -17,15 +17,33 @@ class HomepageForm(forms.ModelForm):
                 attrs={'class': 'form-control', 'placeholder': 'SEO descripton of the cinema'}),
         }
 
-class ContactsForm(forms.ModelForm):
 
+class ContactsForm(forms.ModelForm):
     class Meta:
         model = Contacts
         fields = "__all__"
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name...'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address...'}),
+            'coordinates': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Coordinates...'}),
+            'seo_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'SEO title of the cinema...'}),
+            'seo_keywords': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'SEO keywords of the cinema'}),
+            'seo_description': forms.Textarea(
+                attrs={'class': 'form-control', 'placeholder': 'SEO descripton of the cinema'}),
+        }
 
 
 class PageForm(forms.ModelForm):
-
     class Meta:
         model = Page
         fields = "__all__"
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name...'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description...'}),
+            'seo_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'SEO title of the cinema...'}),
+            'seo_keywords': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'SEO keywords of the cinema'}),
+            'seo_description': forms.Textarea(
+                attrs={'class': 'form-control', 'placeholder': 'SEO descripton of the cinema'}),
+        }
