105870bee97ae9d56f731d3292ef348b10ea229b
ArtemMint <nemnovus@gmail.com>
2021-02-16 22:29:28 +0200

    updated Home, Poster, Premiere, Cinema, CinemaHall, Shares and News html, edit some views and urls.

--- kino/views/site/poster.py
+++ kino/views/site/poster.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.shortcuts import render, redirect, \
+    get_object_or_404, get_list_or_404
 
 from kino.models.film import Film
 from kino.models.pages import HomePage
 
 
-def posterView(request):
+def poster_view(request):
     film_list = Film.objects.all()
     home_page = HomePage.objects.get(id=0)
-
 
     template_name = 'kino/poster.html'
     context = {"film_list": film_list, "home_page": home_page}
--- kino/views/site/shares.py
+++ kino/views/site/shares.py
@@ -2,17 +2,21 @@ from django.views.generic import ListView
 from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
 
 from kino.models.shares import Shares
+from kino.models.image import SharesImage
 
 
-def sharesView(request):
+def shares_view(request):
     shares_list = Shares.objects.all()
 
     context = {"shares_list": shares_list}
     return render(request, 'kino/shares.html', context)
 
 
-def sharesDetailView(request, shares_id):
+def shares_detail_view(request, shares_id):
     shares = get_object_or_404(Shares, id=shares_id)
+    gallery = SharesImage.objects.filter(shares=shares)
 
-    context = {"shares": shares}
-    return render(request,'kino/shares_detail.html', context)
+    template_name = 'kino/shares_detail.html'
+    context = {"shares": shares, gallery: gallery}
+
+    return render(request, template_name, context)
