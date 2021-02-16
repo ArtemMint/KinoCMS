495137f34bfb65aacf0a9cca3454bf6d65312ac5
ivanhumenyuk <vanya19960529@gmail.com>
2021-02-16 11:26:15 +0200

    fix some bugs

--- kino/views/site/home.py
+++ kino/views/site/home.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 
 from kino.models.film import Film
 from kino.models.image import FilmImage
@@ -6,10 +7,12 @@ from kino.models.pages import HomePage
 
 
 def home_page_view(request):
-    
     films = Film.objects.all()
-    gallery = FilmImage.objects.filter(film=1) # should be slider banner on Home page
-    home_page = HomePage.objects.get(id=0)   
+    gallery = FilmImage.objects.filter(film=1)  # should be slider banner on Home page
+    try:
+        home_page = HomePage.objects.get(id=0)
+    except HomePage.DoesNotExist:
+        home_page = None
 
     context = {"films": films, "home_page": home_page, 'gallery': gallery}
     template_name = 'kino/home.html'
