616f2abd120149c050a1f20acb376a8a39cd8b0c
ivanhumenyuk <vanya19960529@gmail.com>
2021-02-21 21:59:49 +0200

    adding mailing success messages, fixin some form bugs

--- kino/repositories/schedule.py
+++ kino/repositories/schedule.py
@@ -1,11 +1,14 @@
+from kino.models import Film
 from kino.models.schedule import Schedule
 
 
 def get_schedule_list_order_by_date_desc() -> list:
     return Schedule.objects.all().order_by('-date')
 
+
 def get_schedule_list_order_by_date_asc() -> list:
     return Schedule.objects.all().order_by('date')
+
 
 def get_schedule_by_id(schedule_id) -> Schedule or None:
     try:
