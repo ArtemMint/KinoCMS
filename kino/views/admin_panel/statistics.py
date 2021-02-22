d64ebd56f8396a3a582e2d72b8c663ce1f09cbd6
Artem Yurchak <nemnovus@gmail.com>
2021-02-22 15:02:19 +0200

    :(

--- kino/views/admin_panel/statistics.py
+++ kino/views/admin_panel/statistics.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect, \
     get_object_or_404, get_list_or_404
 from django.contrib.auth.models import User
 
+from utils import get_avg_age
 from register.models.client import Client
 from kino.models.cinema import Cinema, CinemaHall
 from kino.models.film import Film
 from kino.models.news import News
-from utils import get_avg_age
+from kino.models.shares import Shares
+
 
 
 def adminStatisticsView(request):
@@ -29,9 +31,11 @@ def adminStatisticsView(request):
     num_films = Film.objects.count()
     num_cinemas = Cinema.objects.count()
     num_news = News.objects.count()
+    num_shares = Shares.objects.count()
 
     context = {'users': num_users, 'men': num_men, 'women': num_women,
                'avg_age': avg_age, 'men_avg_age': men_avg_age, 'women_avg_age': women_avg_age,
-               'films': num_films, 'cinemas': num_cinemas, 'num_news': num_news}
+               'films': num_films, 'cinemas': num_cinemas, 'num_news': num_news,
+               'num_shares': num_shares}
 
     return render(request, 'admin_panel/statistics.html', context)
