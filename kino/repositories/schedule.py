1ff862139038ab6f6f9c2156b99e647032b64bdf
Artem Yurchak <nemnovus@gmail.com>
2021-03-01 21:36:43 +0200

    added current day schedule to cinema and cinema hall, fixed some bugs

--- kino/repositories/schedule.py
+++ kino/repositories/schedule.py
@@ -2,15 +2,30 @@ from kino.models.schedule import Schedule
 from utils import get_current_date
 
 
-def get_schedule_list_order_by_date_desc() -> list:
-    return Schedule.objects.filter(date__gte=get_current_date()).order_by('-date')
+def get_schedule_list_order_by_date_desc():
+    return Schedule.objects.filter(
+        date__gte=get_current_date()
+    ).order_by('-date')
 
 
-def get_schedule_list_order_by_date_asc() -> list:
-    return Schedule.objects.filter(date__gte=get_current_date()).order_by('date')
+def get_schedule_list_order_by_date_asc():
+    return Schedule.objects.filter(
+        date__gte=get_current_date()
+    ).order_by('date')
 
 
-def get_schedule_by_id(schedule_id) -> Schedule or None:
+def get_schedule_list_order_by_current_date():
+    return Schedule.objects.filter(date=get_current_date())
+
+
+def get_schedule_list_for_hall_order_by_current_date(cinemahall):
+    return Schedule.objects.filter(
+        date=get_current_date(),
+        cinemahall=cinemahall,
+    )
+
+
+def get_schedule_by_id(schedule_id):
     try:
         schedule = Schedule.objects.get(id=schedule_id)
     except Film.DoesNotExist:
