ac55938c6ac92801d5385c917655ad84f4907b3f
Artem Yurchak <nemnovus@gmail.com>
2021-03-04 12:47:00 +0200

    Update schedule view by right order + fixed Film template

--- kino/repositories/schedule.py
+++ kino/repositories/schedule.py
@@ -3,7 +3,7 @@
 
 from kino.models import Film
 from kino.models.schedule import Schedule
-from utils import get_current_date
+from utils import *
 
 
 def get_schedule_list_order_by_date_desc():
@@ -19,13 +19,13 @@ def get_schedule_list_order_by_date_asc():
     """
     return Schedule.objects.filter(
         date__gte=get_current_date()
-    ).order_by('date')
+    ).order_by('date','time')
 
 
 def get_schedule_list_order_by_current_date():
     """get schedule list by date from DB
     """
-    return Schedule.objects.filter(date=get_current_date())
+    return Schedule.objects.filter(date=get_current_date()).order_by('date')
 
 
 def get_schedule_list_for_hall_order_by_current_date(cinemahall):
@@ -34,7 +34,7 @@ def get_schedule_list_for_hall_order_by_current_date(cinemahall):
     return Schedule.objects.filter(
         date=get_current_date(),
         cinemahall=cinemahall,
-    )
+    ).order_by('date','time')
 
 
 def get_schedule_by_id(schedule_id):
@@ -54,7 +54,11 @@ def get_schedule_by_film_for_each_day(film):
         film ([type]): [description]
     """
     try:
-        schedule = Schedule.objects.filter(film=film, date__gte=get_current_date()).distinct()
+        schedule = Schedule.objects.filter(
+            film=film,
+            date__gt=get_current_date(),
+            date__lte=get_week_ago_date(),
+        ).distinct().order_by('date','time')
     except Schedule.DoesNotExist:
         schedule = None
     return schedule
@@ -67,7 +71,10 @@ def get_today_schedule_by_film(film):
         film ([type]): [description]
     """
     try:
-        schedule = Schedule.objects.filter(film=film, date=get_current_date())
+        schedule = Schedule.objects.filter(
+            film=film,
+            date=get_current_date()
+        ).order_by('date','time')
     except Schedule.DoesNotExist:
         schedule = None
     return schedule
